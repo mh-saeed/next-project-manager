3094d9214d033b722384616a0f2f51e2c6eb661e
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-04-14 01:14:28 +0500

    routes/pages & route groups created

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,7 +10,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div>Layout</div>
+        {children}
+      </body>
     </html>
   );
 }
