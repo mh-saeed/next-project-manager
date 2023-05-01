f6375efbf4e2541ae92330a2cd562c1abb114d5c
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-05-01 22:51:15 +0500

    ProjectCard updated

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -21,6 +21,7 @@ const ProjectCard: FC<{ project: ProjectWithTasks }> = ({ project }) => {
   const completedCount = project.tasks.filter(
     (t) => t.status === "COMPLETED"
   ).length;
+
   const progress = Math.ceil((completedCount / project.tasks.length) * 100);
 
   return (
@@ -30,14 +31,17 @@ const ProjectCard: FC<{ project: ProjectWithTasks }> = ({ project }) => {
           {formatDate(project.createdAt)}
         </span>
       </div>
+
       <div className="mb-6">
         <span className="text-3xl text-gray-600">{project.name}</span>
       </div>
+
       <div className="mb-2">
         <span className="text-gray-400">
           {completedCount}/{project.tasks.length} completed
         </span>
       </div>
+
       <div>
         <div className="w-full h-2 bg-violet-200 rounded-full mb-2">
           <div
@@ -47,6 +51,7 @@ const ProjectCard: FC<{ project: ProjectWithTasks }> = ({ project }) => {
             style={{ width: `${progress}%` }}
           ></div>
         </div>
+
         <div className="text-right">
           <span className="text-sm text-gray-600 font-semibold">
             {progress}%
