eaf8ae3f96cebd7ee2fe8ecac3cd9e3ccbfd1d18
mh-saeed <49342183+mh-saeed@users.noreply.github.com>
2023-05-01 23:02:24 +0500

    fetching projects data completed

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Prisma } from "@prisma/client";
+import { Prisma, TASK_STATUS } from "@prisma/client";
 import Card from "./Card";
 import clsx from "clsx";
 
@@ -9,7 +9,7 @@ const projectWithTasks = Prisma.validator<Prisma.ProjectArgs>()({
 
 type ProjectWithTasks = Prisma.ProjectGetPayload<typeof projectWithTasks>;
 
-const formatDate = (date: any) =>
+const format = (date: any) =>
   new Date(date).toLocaleDateString("en-us", {
     weekday: "long",
     year: "numeric",
@@ -18,8 +18,8 @@ const formatDate = (date: any) =>
   });
 
 const ProjectCard: FC<{ project: ProjectWithTasks }> = ({ project }) => {
-  const completedCount = project.tasks.filter(
-    (t) => t.status === "COMPLETED"
+  const completedCount = project.tasks.map(
+    (project) => project.status === TASK_STATUS.COMPLETED
   ).length;
 
   const progress = Math.ceil((completedCount / project.tasks.length) * 100);
@@ -28,20 +28,17 @@ const ProjectCard: FC<{ project: ProjectWithTasks }> = ({ project }) => {
     <Card className="!px-6 !py-8 hover:scale-105 transition-all ease-in-out duration-200">
       <div>
         <span className="text-sm text-gray-300">
-          {formatDate(project.createdAt)}
+          {format(project.createdAt)}
         </span>
       </div>
-
       <div className="mb-6">
         <span className="text-3xl text-gray-600">{project.name}</span>
       </div>
-
       <div className="mb-2">
         <span className="text-gray-400">
           {completedCount}/{project.tasks.length} completed
         </span>
       </div>
-
       <div>
         <div className="w-full h-2 bg-violet-200 rounded-full mb-2">
           <div
@@ -51,7 +48,6 @@ const ProjectCard: FC<{ project: ProjectWithTasks }> = ({ project }) => {
             style={{ width: `${progress}%` }}
           ></div>
         </div>
-
         <div className="text-right">
           <span className="text-sm text-gray-600 font-semibold">
             {progress}%
